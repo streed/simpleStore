abbc79a16f0497bad1d93336f402c81d97e000bb
streed <sean@cheapfit.me>
2013-09-17 22:01:47 -0400

    Got get to work on cycles as well as forwarding of data when a specific branch does not contain any thje value to a key but at one end the value exists.

--- simpleStore/__init__.py
+++ simpleStore/__init__.py
@@ -1,5 +1,7 @@
 import json
 import requests
+import time
+import uuid
 from flask import Flask, request, jsonify, url_for, g
 from functools import wraps
 from threading import Thread
@@ -12,13 +14,12 @@ def decoder( js ):
 	return d["value"]
 
 def encoder( v ):
-	print v
 	return json.dumps( { "value": v } )
 
 app.encoder = encoder
 app.decoder = decoder
 
-
+packets = {}
 data = {}
 
 def async( f ):
@@ -34,68 +35,54 @@ def get( h, host_str, headers={} ):
 	try:
 		return requests.get( host_str, headers=headers, timeout=5 )
 	except requests.exceptions.ConnectionError:
-		app.logger.debug( "Removing %s from the hosts list, because it appears to be down." % ( h ) )
-		index = app.hosts.index( h )
-		del app.hosts[index]
 		raise
 
-@async
-def distribute_set( args, origin, host ):
-	set_str = "&".join( [ "%s=%s" % ( k, v ) for k, v in args.iteritems() ] )
-	headers = { "Propagate": True, "OriginHost": origin, "LastHost": host }
-	hosts = app.hosts[:]
-	for h in hosts:
-
-		if h != origin and h != host:
-			host_str = "http://%s/set?%s" % ( h, set_str )
-
-			app.logger.debug( "Propagating set to %s" % host_str )
-
-			get( h, host_str, headers=headers )
-
-@async
-def distribute_del( key, origin, host ):
-	headers = { "Propagate": True, "OriginHost": origin, "LastHost": host }
-	hosts = app.hosts[:]
-	for h in hosts:
-		if h != origin and h != host:
-			host_str = "http://%s/del/%s" % ( h, key )
-
-			app.logger.debug( "Propagating delete to %s" % host_str )
-
-			get( h, host_str, headers=headers )
-
-@async
-def distribute_get( key, origin, host ):
+def make_request( h, action, key, origin, host, last, args={} ):
+	args_str = "&".join( [ "%s=%s" % ( k, v ) for k, v in args.iteritems() ] )
 
 	headers = { "Propagate": True, "OriginHost": origin, "LastHost": host }
-	
-	hosts = app.hosts[:]
-	for h in hosts:
-		app.logger.debug( "origin %s" % origin )
-		if h != origin and h != host:
-			host_str = "http://%s/get/%s" % ( h, key )
 
-			result = get( h, host_str, headers=headers )
+	if origin == host:
+		headers["Packet"] = str( uuid.uuid4() )
+		packets[headers["Packet"]] = ( True, int( time.time() ), last )
 
-			if not result.text == "":
-				data[key] = app.decoder( result.text )
-				break
+		headers["Timestamp"] = packets[headers["Packet"]][1]
+
+	host_str = "http://%s/" % h
+	host_str += action
+	if key != "":
+		host_str += "/%s" % key
+	if args_str != "":
+		host_str += "?%s" % args_str
+
+	app.logger.debug( host_str )
+	if h != host:
+
+		return get( h, host_str, headers=headers )
 
 @async
-def send_pings( me ):
-	hosts = app.hosts[:]
-	for h in hosts:
-		host_str = "http://%s/ping?me=%s" % ( h, me )
-		result = get( h, host_str )
-		
-		if not result.text in app.hosts:
-			app.logger.debug( "New host: %s" % result.text )
-			app.hosts.append( result.text )
+def distribute_set( args, origin, host, packet, last ):
+	if packet in packets:
+		return
+	for h in app.hosts:
+		make_request( h, "set", "", origin, host, last, args=args )
 
-#@app.before_first_request
-def send_the_pings():
-	send_pings( request.host )
+@async
+def distribute_del( key, origin, host, packet, last ):
+	if packet in packets:
+		return
+	for h in app.osts:
+		make_request( h, "del", key )
+
+@async
+def distribute_get( key, origin, host, packet, last ):
+	for h in app.hosts:
+		result = make_request( h, "get", key, origin, host, last )
+		app.logger.debug( result.text )
+		if result and result.text != "":
+			data[key] = app.decoder( result.text )
+			make_request( last, "fwd", "", origin, host, last, args={ "key": key, "value": data[key], "packet": packet } )
+			break
 
 @app.route( "/ping" )
 def ping():
@@ -108,7 +95,7 @@ def ping():
 
 @app.route( "/dump" )
 def dump():
-	return app.encoder( data )
+	return app.encoder( [ data, packets ] )
 
 @app.route( "/set" )
 def set_key():
@@ -116,7 +103,6 @@ def set_key():
 	changes = {}
 
 	for k, v in request.args.iteritems():
-		app.logger.debug( "Setting %s => %s" % ( k, v ) )
 		if k not in data:
 			changes[k] = v
 			data[k] = v
@@ -127,14 +113,15 @@ def set_key():
 
 
 	if changes != {}:
-		app.logger.debug( "Should we propagate the set value?" )
 		if not "Propagate" in request.headers: 
-			app.logger.debug( "No `Propagate` in headers so let's start the propagation." )
-			distribute_set( changes, request.host, request.host )
+			distribute_set( changes, request.host, request.host, "", "" )
 		else:
-			app.logger.debug( "There was a `Propagate` in the headers so there must be a `OriginHost` as well so lets grab it and then start propagating." )
 			origin = request.headers.get( "OriginHost" )
-			distribute_set( changes, origin, request.host )
+			packet = request.headers.get( "Packet" )
+			last = request.headers.get( "LastHost" )
+			timestamp = request.headers.get( "Timestamp" )
+
+			distribute_set( changes, origin, request.host, packet, last )
 
 	return ""
 
@@ -142,19 +129,20 @@ def set_key():
 @app.route( "/get/<key>" )
 def get_key( key ):
 
-	app.logger.debug( "Trying to get the value for %s" % key )
 	if key in data:
 		return app.encoder( data[key] )
 	else:
-		app.logger.debug( "The key did not exist so lets try and grab it from the other nodes." )
-		app.logger.debug( "Shold we propagate the set value?" )
 		if not "Propagate" in request.headers: 
-			app.logger.debug( "No `Propagate` in headers so let's start the propagation." )
-			distribute_get( key, request.host, request.host )
+			distribute_get( key, request.host, request.host, "", "" )
 		else:
-			app.logger.debug( "There was a `Propagate` in the headers so there must be a `OriginHost` as well so lets grab it and then start propagating." )
 			origin = request.headers.get( "OriginHost" )
-			distribute_get( key, origin, request.host )
+			packet = request.headers.get( "Packet" )
+			last = request.headers.get( "LastHost" )
+			timestamp = request.headers.get( "Timestamp" )
+
+			if not packet in packets:
+				packets[packet] = ( True, timestamp, last )
+				distribute_get( key, origin, request.host, packet, last )
 
 	return ""
 
@@ -163,16 +151,31 @@ def del_key( key ):
 	if key in data:
 		del data[key]
 
-		app.logger.debug( "Should we propagate the del value?" )
 		if not "Propagate" in request.headers: 
-			app.logger.debug( "No `Propagate` in headers so let's start the propagation." )
-			distribute_del( key, request.host, request.host )
+			distribute_del( key, request.host, request.host, "", "" )
 		else:
-			app.logger.debug( "There was a `Propagate` in the headers so there must be a `OriginHost` as well so lets grab it and then start propagating." )
 			origin = request.headers.get( "OriginHost" )
+			packet = request.headers.get( "Packet" )
+			last = request.headers.get( "LastHost" )
+			timestamp = request.headers.get( "Timestamp" )
 
-			app.logger.debug( "OriginHost: %s" % origin )
-			distribute_del( key, origin, request.host )
+			distribute_del( key, origin, request.host, packet, last )
 
 	return ""
 
+@app.route( "/fwd" )
+def fwd_key():
+	packet = request.args.get( "packet" )
+	key = request.args.get( "key" )
+	value = request.args.get( "value" )
+
+	if packet in packets:
+		last = packets[packet][2]
+
+		del packets[packet]
+
+		data[key] = value
+		if last != "":
+			make_request( last, "fwd", "", request.host, "", { "key": key, "value": value, "packet": packet } )
+
+	return ""
